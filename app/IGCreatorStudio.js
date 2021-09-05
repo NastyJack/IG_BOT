a6ac921d9d6b23fdfd6499e84b7f8332384ba57f
Rishabh Nishad <rishabh@mordorintelligence.com>
2021-09-05 16:45:57 +0530

    enabled post flag at script

--- app/IGCreatorStudio.js
+++ app/IGCreatorStudio.js
@@ -116,10 +116,10 @@ let page,
         );
         await page.keyboard.type(postThis.title, { delay: 80 });
 
-        // await Helpers.ClickButton(
-        //   page,
-        //   `/html/body/div[4]/div/div/div/div[3]/div[2]/button`
-        // );
+        await Helpers.ClickButton(
+          page,
+          `/html/body/div[4]/div/div/div/div[3]/div[2]/button`
+        );
       } catch (e) {
         console.log("Error occured at CreatorStudio", e);
       }
